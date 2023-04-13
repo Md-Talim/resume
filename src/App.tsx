d8d55382b1c26a02473295a98376ec83b42f61fa
Mohammad Talim <talimmohammad116@gmail.com>
2023-04-13 19:24:18 +0530

    remove the profile section to make a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import { About, Education, Projects } from './sections';
-import { UnderlinedLink } from './components';
-import { talim } from './assets';
-import Contact from './sections/Contact';
+import { Profile, About, Education, Projects, Contact } from './sections';
 
 const App = () => {
   return (
     <main className="mx-4 sm:mx-auto my-20 max-w-lg">
-      <section className="flex items-center justify-start gap-4">
-        <figure>
-          <img
-            src={talim}
-            alt="talim"
-            className="aspect-square w-20 rounded-full"
-          />
-        </figure>
-        <div>
-          <h1 className="text-xl text-gray1">Mohammad Talim</h1>
-          <p>
-            2nd Year CSE Student at{' '}
-            <UnderlinedLink
-              title="GP Indri"
-              link="https://gpindri.ac.in/"
-              color="light"
-            />
-          </p>
-          <UnderlinedLink
-            title="md-talim.github.io"
-            link="md-talim.github.io"
-            color={'dark' as string}
-          />
-        </div>
-      </section>
-
+      <Profile />
       <About />
       <Education />
       <Projects />
